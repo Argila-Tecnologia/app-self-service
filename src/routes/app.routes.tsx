72499d37c039fc10f5c0aba726e7200e9584b4c0
Lubni Morais <lubni.morais@gmail.com>
2024-08-22 20:53:36 -0300

    bug: error react native navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,19 +2,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { HomeScreen } from '@screens/Home';
 import { DetailProductScreen } from '@screens/DetailProduct';
-import { Test } from '@screens/Test';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
   return (
     <Navigator
-      initialRouteName="tes"
+      initialRouteName="homeScreen"
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Screen name="tes" component={Test} />
       <Screen name="homeScreen" component={HomeScreen} />
       <Screen name="detailProductScreen" component={DetailProductScreen} />
     </Navigator>
